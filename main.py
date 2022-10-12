f16262f1c8aff0cc92e363006a938d120afb0bd2
niksveidemanis <niks.veidemanis@inbox.lv>
2022-10-12 15:56:33 +0300

    Pievienoju layoutu, main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, json, jsonify, redirect, session, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+app.run(host="127.0.0.1", port=8080, debug=True)
