bd2acf07749f59dbbb7905b785488b0413e9a1eb
defaulltss <ralfs.daniels.uks@gmail.com>
2022-10-12 22:07:29 +0300

    ir mazs issue ar redirect un python, rit salabosu

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, json, jsonify, redirect, session, url_for
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
