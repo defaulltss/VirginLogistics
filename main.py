06c49255522f6ba2ccca4f678c8c1b5743bac316
defaulltss <ralfs.daniels.uks@gmail.com>
2022-10-13 13:26:40 +0300

    pieliku atsevisku sadalu par mums, still vajag daudz ko labot, nemu pauzi

--- main.py
+++ main.py
@@ -21,5 +21,20 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/bill')
+def bill():
+    return render_template('bill.html')
+
+@app.route('/listings_view')
+def listings_view():
+    return render_template('listings_view.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 app.run(host="127.0.0.1", port=8080, debug=True)
