ee436d6fd7e2ebef09d66a2e4514ccd7152be781
Anon <kautkaadsepasts80@gmail.com>
2022-11-01 17:30:45 +0200

    Pievienot sludinajumus

---
+++ actions.php
@@ -0,0 +1,35 @@
+<?php
+# Pievienot, rediģēt un dzēst sludinājumus
+include "template/access.php";
+include "header.php";
+?>
+
+<html>
+<button id="ievadeee" name="pievienot" onclick="document.getElementById('ievadeee').style.display = 'none';document.getElementById('ievadee').style.display = ''">Pievienot Sludinājumu</button> <!-- Nospiežot, parādās ievades logs | Divi "e" burti ID ir dizaina specifika, lai nerastos problēmas ar .css failu -->
+
+<div id="ievadee" style="display:none">
+  <!-- Pievieno jaunu sludinājumu 
+  FORMAI VAJAG action="" UN method=""
+-->
+  <form onsubmit="this.reset()">
+    <label for="myfile"> Pievienot bildi: </label>
+    <input type="file" id="myfile" name="myfile" />
+    <br></br>
+    <label>Apraksts :</label>
+    <input type="text" name="apraksts" placeholder="Tīrs, mājīgs dzīvoklis" required>
+    <br></br>
+    <label>cena:</label>
+    <input type="text" name="Cena" placeholder="50" required> <label>€/mēn</label>
+  </form>
+  <br>
+  <button name="nosutit" onclick="document.getElementById('ievadeee').style.display = '';document.getElementById('ievadee').style.display = 'none'">Nosūtīt</button>
+</div>
+
+<div class="sludinajumi">
+
+    *Te parādīsies sludinājumi*
+    <br>
+    (man vajag šo tekstu redzēt)
+
+</div>
+</html>
