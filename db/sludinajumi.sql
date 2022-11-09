a3971b026a73058669aa7860200eeb7561cb90d1
Anon <kautkaadsepasts80@gmail.com>
2022-11-09 21:10:00 +0200

    sludinajumu table

---
+++ db/sludinajumi.sql
@@ -0,0 +1,14 @@
+-- php admin > project db > query (this) > submit query
+-- paldies V. kungam par query garantijas remontu
+
+CREATE TABLE ipasumi (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nosaukums VARCHAR(50) NOT NULL,
+    cena INT NOT NULL,
+    vieta VARCHAR(320) NOT NULL,
+    stavs INT NOT NULL,
+    istabskaits INT NOT NULL,
+    platiba INT NOT NULL,
+    apraksts VARCHAR(320) NOT NULL,
+    type INT(11) NULL Default '0'
+);
